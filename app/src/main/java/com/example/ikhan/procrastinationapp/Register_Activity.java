517d8dd5ff6fb736e29aa0d133ce4995d6d84432
Saad Khan <saad@thefastbite.com>
2018-01-06 23:13:27 -0500

    Login and Register Feature

--- app/src/main/java/com/example/ikhan/procrastinationapp/Register_Activity.java
+++ app/src/main/java/com/example/ikhan/procrastinationapp/Register_Activity.java
@@ -1,55 +1,53 @@
 package com.example.ikhan.procrastinationapp;
 
 import android.app.Activity;
-import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.android.volley.RequestQueue;
 import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class MainActivity extends Activity {
+public class Register_Activity extends Activity {
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_register_);
+        Button register=(Button)findViewById(R.id.Register);
+        final TextView etName=(TextView)findViewById(R.id.Name);
         final TextView etUsername=(TextView)findViewById(R.id.Username);
         final TextView etPassword=(TextView)findViewById(R.id.Password);
-        final Button login=(Button)findViewById(R.id.Login);
-        final Button signup=(Button)findViewById(R.id.SignUp);
-        signup.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent=new Intent(MainActivity.this, Register_Activity.class);
-                MainActivity.this.startActivity(intent);
-            }
-        });
-        login.setOnClickListener(new View.OnClickListener() {
+
+        register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 final String username=etUsername.getText().toString();
                 final String password=etPassword.getText().toString();
+                final String name=etName.getText().toString();
+
                 Response.Listener<String>responseListener=new Response.Listener<String>(){
 
                     @Override
                     public void onResponse(String response) {
                         try {
                             JSONObject jsonObject=new JSONObject(response);
-                            boolean success= jsonObject.getBoolean("success");
+                            boolean success=jsonObject.getBoolean("success");
                             if (success){
-                                String name=jsonObject.getString("name");
+                                Intent intent=new Intent(Register_Activity.this, MainActivity.class);
+                                Register_Activity.this.startActivity(intent);
 
-
-                            }else{
-                                AlertDialog.Builder builder=new AlertDialog.Builder(MainActivity.this);
+                            }
+                            else{
+                                AlertDialog.Builder builder=new AlertDialog.Builder(Register_Activity.this);
                                 builder.setMessage("Register Failed")
                                         .setNegativeButton("Retry", null)
                                         .create()
@@ -58,14 +56,15 @@ public class MainActivity extends Activity {
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
-                        
                     }
                 };
-                //LoginRequest loginRequest=new LoginRequest(username, password);
 
-
-
+                RegisterRequest registerRequest=new RegisterRequest(name, username,password,responseListener );
+                RequestQueue queue= Volley.newRequestQueue(Register_Activity.this);
+                queue.add(registerRequest);
             }
         });
+
+
     }
 }
